06e1a0f5f9c5abc03722812e4f95ad03348a87e8
ArTyy022 <toledoarthur11@gmail.com>
2025-08-17 16:03:08 -0300

    Add files via upload

--- src/ExercicioQuatro.java
+++ src/ExercicioQuatro.java
@@ -4,8 +4,21 @@ public class ExercicioQuatro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("informe o número inicial");
+        System.out.println("informe o número inicial: ");
         int numeroInicial = scanner.nextInt();
-        
+
+        while (true) {
+            System.out.println("Informe um número: ");
+            int numero = scanner.nextInt();
+
+            if (numero < numeroInicial) {
+                System.out.println("Número ignorado (menor que o inicial)."); continue;
+            }
+            if (numero % numeroInicial != 0) {
+                System.out.println("Número não divisível por " + numeroInicial + ". Encerrando...");
+                break;
+            }
+            System.out.println("Número válido: " + numero + " (divisível por " + numeroInicial + ")");
+        }
     }
 }
