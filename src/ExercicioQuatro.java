35c409229db77ec22c03bdf0082d6620f55faa6a
ArTyy022 <toledoarthur11@gmail.com>
2025-08-17 15:17:19 -0300

    Add files via upload

---
+++ src/ExercicioQuatro.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("informe o número inicial");
+        int numeroInicial = scanner.nextInt();
+        
+    }
+}
